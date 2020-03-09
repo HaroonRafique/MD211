f35b994abafb5bf03d974978a09d83f76e6fe48d
Haroon Rafique <haroon.rafique@protonmail.com>
2020-03-09 15:06:07 +0100

    02: updated

--- 02_Initial_Setup_Parabolic/create_new_scan.sh
+++ 02_Initial_Setup_Parabolic/create_new_scan.sh
@@ -8,10 +8,8 @@ cp -r ${folder_in} ${folder_out}_02
 cp -r ${folder_in} ${folder_out}_03
 cp -r ${folder_in} ${folder_out}_04
 
-folder_out=01
-
-cp -r ${folder_in} ${folder_out}_00
-cp -r ${folder_in} ${folder_out}_01
-cp -r ${folder_in} ${folder_out}_02
-cp -r ${folder_in} ${folder_out}_03
-cp -r ${folder_in} ${folder_out}_04
+cp -r ${folder_in} ${folder_out}_10
+cp -r ${folder_in} ${folder_out}_11
+cp -r ${folder_in} ${folder_out}_12
+cp -r ${folder_in} ${folder_out}_13
+cp -r ${folder_in} ${folder_out}_14
