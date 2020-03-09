e03736b93005dc731ef89bd119d4f92e6011f99f
Haroon Rafique <haroon.rafique@protonmail.com>
2020-03-09 13:08:38 +0100

    02_Initial_Setup_Parabolic/ added

---
+++ 02_Initial_Setup_Parabolic/create_new_scan.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+folder_in=00_Master
+folder_out=00
+
+cp -r ${folder_in} ${folder_out}_00
+cp -r ${folder_in} ${folder_out}_01
+cp -r ${folder_in} ${folder_out}_02
+cp -r ${folder_in} ${folder_out}_03
+cp -r ${folder_in} ${folder_out}_04
+
+folder_out=01
+
+cp -r ${folder_in} ${folder_out}_00
+cp -r ${folder_in} ${folder_out}_01
+cp -r ${folder_in} ${folder_out}_02
+cp -r ${folder_in} ${folder_out}_03
+cp -r ${folder_in} ${folder_out}_04
