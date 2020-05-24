d57e5e7d0d44c45a365f265943e52408c07ebb04
Haroon Rafique <haroon.rafique@protonmail.com>
2020-05-25 01:44:54 +0200

    08: D mismatch injection bump added

---
+++ 08_Dipersion_Mismatch_Injection_Bump/create_new_scan.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+folder_in=00_Master
+folder_out=1
+
+cp -r ${folder_in} ${folder_out}_00
+cp -r ${folder_in} ${folder_out}_01
+cp -r ${folder_in} ${folder_out}_02
+cp -r ${folder_in} ${folder_out}_03
+cp -r ${folder_in} ${folder_out}_04
+
+cp -r ${folder_in} ${folder_out}_10
+cp -r ${folder_in} ${folder_out}_11
+cp -r ${folder_in} ${folder_out}_12
+cp -r ${folder_in} ${folder_out}_13
+cp -r ${folder_in} ${folder_out}_14
