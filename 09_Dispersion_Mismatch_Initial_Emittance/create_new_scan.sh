9e534267d51eb9c48a38f65b02adb9ccc571469b
Haroon Rafique <haroon.rafique@protonmail.com>
2020-07-13 11:00:49 +0200

    09: added to test different initial emittances

---
+++ 09_Dispersion_Mismatch_Initial_Emittance/create_new_scan.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+folder_in=00_Master
+folder_out=1
+
+#cp -r ${folder_in} ${folder_out}_004 # Already simulated
+cp -r ${folder_in} ${folder_out}_104
+cp -r ${folder_in} ${folder_out}_204
+cp -r ${folder_in} ${folder_out}_304
+cp -r ${folder_in} ${folder_out}_404
+
+#cp -r ${folder_in} ${folder_out}_014 # Already simulated
+cp -r ${folder_in} ${folder_out}_114
+cp -r ${folder_in} ${folder_out}_214
+cp -r ${folder_in} ${folder_out}_314
+cp -r ${folder_in} ${folder_out}_414
